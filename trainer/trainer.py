7bd313ea166335d82882c94d53182382d29b21ca
Re-bin <1718954198@qq.com>
2023-02-09 15:19:06 +0800

    update trainer.py

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -3,21 +3,44 @@ import time
 import torch
 import torch.optim as optim
 
+from tqdm import tqdm
 from config.configurator import configs
 
 print(configs)
 
 class Trainer(object):
-    def __init__(self):
-        pass
+    def __init__(self, data_handler):
+        self.data_handler = data_handler
 
     def create_optimizer(self, model):
         optim_config = configs['optimizer']
         if optim_config['name'] == 'adam':
             self.optimizer = optim.Adam(model.parameters(), lr=optim_config['lr'], weight_decay=optim_config['weight_decay'])
 
-    def train_epoch(self, epoch_idx):
-        pass
+    def train_epoch(self, model, epoch_idx):
+        # prepare training data
+        train_dataloader = self.data_handler.train_dataloader
+        train_dataloader.neg_sampling()
+
+        # loss log
+        loss_log_dict = {}
+
+        # start
+        model.train()
+        for _, tem in enumerate(train_dataloader):
+            self.optimizer.zero_grad()
+            batch_data = list(map(lambda x: x.long().cuda()), tem)
+            loss, loss_dict = model.cal_loss(batch_data)
+            loss.backward()
+            self.optimizer.step()
+
+            # for log
+            for loss_name in loss_dict:
+                _loss_val = float(loss_dict[loss_name]) / len(train_dataloader)
+                if loss_name not in loss_dict: loss_log_dict[loss_name] = _loss_val
+                else: loss_log_dict[loss_name] += _loss_val
+
+        # log
 
     def train(self, model):
         train_config = configs['train']
@@ -25,8 +48,8 @@ class Trainer(object):
             self.train_epoch(epoch_idx)
         self.save_model(model)
 
-    def evaluate(self):
-        pass
+    def evaluate(self, model):
+        model.eval()
 
     def save_model(self, model):
         if configs['trian']['save_model']:
