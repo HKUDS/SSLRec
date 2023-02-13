dd68b5f75571a1232f64c2cfd1d74419e66b8f24
WW <39010092+weiwei1206@users.noreply.github.com>
2023-02-13 13:13:49 +0800

    Update build_trainer.py

--- trainer/build_trainer.py
+++ trainer/build_trainer.py
@@ -1,5 +1,5 @@
 from .trainer import *
-from .trainer_cml import *
+from .trainer_multi_behavior import *
 from config.configurator import configs
 
 def build_trainer(data_handler, logger):
@@ -7,6 +7,10 @@ def build_trainer(data_handler, logger):
         trainer = Trainer(data_handler, logger)
     elif configs['train']['trainer'] == 'cml_trainer':
         trainer = CMLTrainer(data_handler, logger)
+     elif configs['train']['trainer'] == 'mmclr_trainer':
+        trainer = MMCLRTrainer(data_handler, logger)
     else:
         raise NotImplementedError
     return trainer
+
+
