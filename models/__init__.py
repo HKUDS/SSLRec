9b15d3818b82a99938ce5a089c03205507e6d245
WW <39010092+weiwei1206@users.noreply.github.com>
2023-02-13 13:09:43 +0800

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,4 @@ from models.general_cf.lightgcn import LightGCN
 from models.general_cf.sgl import SGL
 from models.multi_behavior.cml import CML, MetaWeightNet
 from models.social.smin import SMIN
+from models.multi_behavior.mmclr import MMCLR
