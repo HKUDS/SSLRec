c2cef84a4c05d6371739a1718e1c90106adb9110
louiswng <louiswong.cs@connect.hku.hk>
2023-02-13 14:49:07 +0800

    add Informax

--- models/model_utils.py
+++ models/model_utils.py
@@ -28,3 +28,92 @@ class NodeDrop(nn.Module):
 		node_num = data_config['user_num'] + data_config['item_num']
 		mask = (t.rand(node_num) + keep_rate).floor().view([-1, 1])
 		return embeds * mask
+
+class GraphConv(nn.Module):
+    def __init__(self,
+                 in_feats,
+                 out_feats,
+                 weight=False,
+                 activation=None):
+        super(GraphConv, self).__init__()
+        self._in_feats = in_feats
+        self._out_feats = out_feats
+        self._norm = "both"
+        if weight:
+            self.weight = nn.Parameter(th.Tensor(in_feats, out_feats))
+        else:
+            self.register_parameter('weight', None)
+
+        self.reset_parameters()
+
+        self._activation = activation
+
+    def reset_parameters(self):
+        """Reinitialize learnable parameters."""
+        if self.weight is not None:
+            init.xavier_uniform_(self.weight)
+
+    def forward(self, graph, feat, weight=None):
+        graph = graph.local_var()
+
+        if self._norm == 'both':
+            degs = graph.out_degrees().to(feat.device).float().clamp(min=1) # outdegree of nodes
+            norm = th.pow(degs, -0.5)
+            shp = norm.shape + (1,) * (feat.dim() - 1) # (n, 1)
+            norm = th.reshape(norm, shp) # (n, 1)
+            # feat = feat * norm
+
+        if weight is not None:
+            if self.weight is not None:
+                raise DGLError('External weight is provided while at the same time the'
+                               ' module has defined its own weight parameter. Please'
+                               ' create the module with flag weight=False.')
+        else:
+            weight = self.weight
+
+        if self._in_feats > self._out_feats:
+            # mult W first to reduce the feature size for aggregation.
+            if weight is not None:
+                feat = th.matmul(feat, weight)
+            feat = feat * norm
+            graph.srcdata['h'] = feat
+            graph.update_all(fn.copy_u(u='h', out='m'),
+                             fn.sum(msg='m', out='h'))
+            rst = graph.dstdata['h']
+        else:
+            # aggregate first then mult W
+            graph.srcdata['h'] = feat
+            graph.update_all(fn.copy_u(u='h', out='m'),
+                             fn.sum(msg='m', out='h'))
+            rst = graph.dstdata['h']
+            if weight is not None:
+                rst = th.matmul(rst, weight)
+
+        if self._norm != 'none':
+            degs = graph.in_degrees().to(feat.device).float().clamp(min=1)
+            if self._norm == 'both':
+                norm = th.pow(degs, -0.5)
+            else:
+                norm = 1.0 / degs
+            shp = norm.shape + (1,) * (feat.dim() - 1)
+            norm = th.reshape(norm, shp)
+            rst = rst * norm
+        if self._activation is not None:
+            rst = self._activation(rst)
+
+        return rst
+
+class GCN(nn.Module):
+    def __init__(self,
+                 g,
+                 in_feats,
+                 n_hidden,
+                 activation):
+        super(GCN, self).__init__()
+        self.g = g
+        self.layer = GraphConv(in_feats, n_hidden, weight=False, activation=activation)
+
+    def forward(self, features):
+        h = features
+        h = self.layer(self.g, h)
+        return h
--- models/social/smin.py
+++ models/social/smin.py
@@ -1,11 +1,12 @@
 import torch as t
 from torch import nn
 import torch.nn.functional as F
+from dgl.nn.pytorch import GraphConv
+import numpy as np
 from config.configurator import configs
 from models.loss_utils import cal_bpr_loss, reg_pick_embeds
 from models.base_model import BaseModel
-from models.model_utils import SpAdjEdgeDrop
-from dgl.nn.pytorch import GraphConv
+from models.model_utils import GCN
 
 init = nn.init.xavier_uniform_
 uniformInit = nn.init.uniform
@@ -19,8 +20,6 @@ class SMIN(BaseModel):
 		self.user_graph_indx = configs['model']['user_graph_indx']
 		self.item_graph_indx = configs['model']['item_graph_indx']
 		self.gcn_act = configs['model']['gcn_act']
-		self.informax = configs['model']['informax']
-		self.informax_graph_act = configs['model']['informax_graph_act']
 		self.lambda1 = configs['model']['lambda1']
 		self.lambda2 = configs['model']['lambda2']
 		self.k_hop_num = configs['model']['k_hop_num']
@@ -48,9 +47,13 @@ class SMIN(BaseModel):
 				item_layers.append(GraphConv(self.embedding_size,self.embedding_size, bias=False, activation=self.act))
 			self.item_meta_layers.append(item_layers)
 		
+
 		self.semantic_user_attn = SemanticAttention(self.embedding_size)
 		self.semantic_item_attn = SemanticAttention(self.embedding_size)
-	
+
+		informax_graph_act = nn.Sigmoid()
+		self.ui_informax = Informax(data_handler.ui_graph, self.embedding_size, self.embedding_size, nn.PReLU, informax_graph_act, data_handler.ui_graph_adj).cuda()
+
 	def _propagate(self, adj, embeds):
 		return t.spmm(adj, embeds)
 	
@@ -104,3 +107,62 @@ class SemanticAttention(nn.Module):
         beta = t.softmax(w, dim=0)
         beta = beta.expand((z.shape[0],)+beta.shape) 
         return (beta*z).sum(1)
+
+class Encoder(nn.Module):
+    def __init__(self, g, in_feats, n_hidden, activation):
+        super(Encoder, self).__init__()
+        self.g = g
+        self.conv = GCN(g, in_feats, n_hidden, activation)
+
+    def forward(self, features, corrupt=False):
+        if corrupt:
+            perm = torch.randperm(self.g.number_of_nodes())
+            features = features[perm]
+        features = self.conv(features)
+        return features
+
+class Discriminator(nn.Module):
+    def __init__(self, n_hidden):
+        super(Discriminator, self).__init__()
+        self.weight = nn.Parameter(nn.init.xavier_uniform_(t.empty(n_hidden, n_hidden)))
+        self.loss = nn.BCEWithLogitsLoss(reduction='none') # combines a Sigmoid layer and the BCELoss
+
+    def forward(self,node_embedding,graph_embedding, corrupt=False):
+        score = torch.sum(node_embedding*graph_embedding,dim=1) 
+        
+        if corrupt:
+            res = self.loss(score,torch.zeros_like(score))
+        else:
+            res = self.loss(score,torch.ones_like(score))
+        return res
+
+class Informax(nn.Module):
+	def __init__(self, g, n_in, n_h, gcn_act, graph_act, graph_adj):
+		super(Informax, self).__init__()
+		self.encoder = Encoder(g, n_in, n_h, gcn_act)
+		self.discriminator = Discriminator(n_h)
+		self.graph_act = graph_act
+		graph_adj_coo = graph_adj.tocoo()
+		graph_adj_u, graph_adj_v, graph_adj_r = graph_adj_coo.row, graph_adj_coo.col, graph_adj_coo.data
+		self.graph_adj_data = np.hstack((graph_adj_u.reshape(-1, 1), graph_adj_v.reshape(-1, 1))).tolist()
+		self.graph_adj_data = np.array(self.graph_adj_data)
+		self.mse_loss = nn.MSELoss(reduction='sum')
+	
+	def forward(self, features, subgraph_adj, subgraph_adj_tensor,subgraph_adj_norm):
+		positive = self.encoder(features, corrupt=False)
+		negative = self.encoder(features, corrupt=True)
+
+		tmp_features = features
+		graph_embeds = torch.sparse.mm(subgraph_adj_tensor, tmp_features) / subgraph_adj_norm
+		graph_embeds = self.graph_act(graph_embeds)
+
+		pos_hi_xj_loss = self.discriminator(positive, graph_embeds, corrupt=False)
+		neg_hi_xj_loss = self.discriminator(negative, graph_embeds, corrupt=True)
+
+		pos_hi_xi_loss = self.discriminator(positive, features, corrupt=False)
+		neg_hi_xi_loss = self.discriminator(negative, features, corrupt=True)
+
+		tmp = t.sigmoid(t.sum(positive[self.graph_adj_data[:, 0]] * positive[self.graph_adj_data[:,1]], dim=1))
+		adj_rebuilt = self.mse_loss(tmp, t.ones_like(tmp)) / positive.shape[0]
+
+		return pos_hi_xj_loss, neg_hi_xj_loss, pos_hi_xi_loss, neg_hi_xi_loss, adj_rebuilt
