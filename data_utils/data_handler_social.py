55568eca80b3dc61603a6d3498211aa85ddb698f
louiswng <louiswong.cs@connect.hku.hk>
2023-02-13 10:43:59 +0800

    load data

--- data_utils/data_handler_social.py
+++ data_utils/data_handler_social.py
@@ -15,66 +15,20 @@ class DataHandlerSocial:
 			predir = './datasets/social_epinions/'
 		elif configs['data']['name'] == 'yelp':
 			predir = './datasets/social_yelp/'
-		self.rating_file = predir + 'rating.mat'
-		self.trust_file = predir + 'trustnetwork.mat'
-
-	def _load_one_mat(self, file):
-		"""Load one single adjacent matrix from file
-
-		Args:
-			file (string): path of the file to load
-
-		Returns:
-			scipy.sparse.coo_matrix: the loaded adjacent matrix
-		"""
-		with open(file, 'rb') as fs:
-			mat = (pickle.load(fs) != 0).astype(np.float32)
-		if type(mat) != coo_matrix:
-			mat = coo_matrix(mat)
-		return mat
-	
-	def _normalize_adj(self, mat):
-		"""Laplacian normalization for mat in coo_matrix
-
-		Args:
-			mat (scipy.sparse.coo_matrix): the un-normalized adjacent matrix
-
-		Returns:
-			scipy.sparse.coo_matrix: normalized adjacent matrix
-		"""
-		degree = np.array(mat.sum(axis=-1))
-		d_inv_sqrt = np.reshape(np.power(degree, -0.5), [-1])
-		d_inv_sqrt[np.isinf(d_inv_sqrt)] = 0.0
-		d_inv_sqrt_mat = sp.diags(d_inv_sqrt)
-		return mat.dot(d_inv_sqrt_mat).transpose().dot(d_inv_sqrt_mat).tocoo()
-	
-	def _make_torch_adj(self, mat):
-		"""Transform uni-directional adjacent matrix in coo_matrix into bi-directional adjacent matrix in torch.sparse.FloatTensor
-
-		Args:
-			mat (coo_matrix): the uni-directional adjacent matrix
-
-		Returns:
-			torch.sparse.FloatTensor: the bi-directional matrix
-		"""
-		a = csr_matrix((configs['data']['user_num'], configs['data']['user_num']))
-		b = csr_matrix((configs['data']['item_num'], configs['data']['item_num']))
-		mat = sp.vstack([sp.hstack([a, mat]), sp.hstack([mat.transpose(), b])])
-		mat = (mat != 0) * 1.0
-		# mat = (mat + sp.eye(mat.shape[0])) * 1.0# MARK
-		mat = self._normalize_adj(mat)
-
-		# make torch tensor
-		idxs = t.from_numpy(np.vstack([mat.row, mat.col]).astype(np.int64))
-		vals = t.from_numpy(mat.data.astype(np.float32))
-		shape = t.Size(mat.shape)
-		return t.sparse.FloatTensor(idxs, vals, shape).cuda()
+		self.data_file = predir + 'data.pkl'
+		self.metapath_file = predir + 'metapath.pkl'
+		self.subgraph_file = predir + '2hop_ui_subgraph.pkl'
 	
 	def load_data(self):
-		trn_mat = self._load_one_mat(self.trn_file)
-		tst_mat = self._load_one_mat(self.tst_file)
+		with open(self.data_file, 'rb') as fs:
+			data = pickle.load(fs)
+		with open(self.metapath_file, 'rb') as fs:
+			metapath = pickle.load(fs)
+		with open(self.subgraph_file, 'rb') as fs:
+			subgraph = pickle.load(fs)
         
-		self.trn_mat = trn_mat
+		trn_mat, tst_data, _, _, _ = data
+		print(trn_mat)
 		configs['data']['user_num'], configs['data']['item_num'] = trn_mat.shape
 		self.torch_adj = self._make_torch_adj(trn_mat)
 
