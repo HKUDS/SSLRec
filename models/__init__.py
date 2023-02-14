424bf8565327926f76ad769fecc006a1f8b8a9fa
WW <39010092+weiwei1206@users.noreply.github.com>
2023-02-14 21:56:12 +0800

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -2,9 +2,11 @@ from models.general_cf.lightgcn import LightGCN
 from models.general_cf.sgl import SGL
 from models.general_cf.lightgcl import LightGCL
 from models.general_cf.hccf import HCCF
-from models.multi_behavior.cml import CML, MetaWeightNet
 from models.social.smin import SMIN
 from models.social.kcgn import KCGN
-from models.multi_behavior.mmclr import MMCLR
 from models.sequential.bert4rec import BERT4Rec
 from models.sequential.cl4srec import CL4SRec
+from models.multi_behavior.hmgcr import HMGCR
+from models.multi_behavior.smbrec import SMBRec
+from models.multi_behavior.cml import CML, MetaWeightNet
+from models.multi_behavior.mmclr import MMCLR
