19839f1baff4a2c2f83a0d4f8049b55c542cac99
akaxlh <767256483@qq.com>
2023-02-18 20:09:29 +0800

    update simgcl

--- models/general_cf/simgcl.py
+++ models/general_cf/simgcl.py
@@ -3,7 +3,7 @@ from torch import nn
 import torch.nn.functional as F
 from config.configurator import configs
 from models.general_cf.lightgcn import LightGCN
-from models.loss_utils import cal_bpr_loss, reg_pick_embeds, cal_infonce_loss
+from models.loss_utils import cal_bpr_loss, reg_params, cal_infonce_loss
 
 init = nn.init.xavier_uniform_
 uniformInit = nn.init.uniform
@@ -48,9 +48,10 @@ class SimGCL(LightGCN):
 		anc_embeds1, pos_embeds1, neg_embeds1 = self._pick_embeds(user_embeds1, item_embeds1, batch_data)
 		anc_embeds2, pos_embeds2, neg_embeds2 = self._pick_embeds(user_embeds2, item_embeds2, batch_data)
 		anc_embeds3, pos_embeds3, neg_embeds3 = self._pick_embeds(user_embeds3, item_embeds3, batch_data)
-		bpr_loss = cal_bpr_loss(anc_embeds3, pos_embeds3, neg_embeds3)
+		bpr_loss = cal_bpr_loss(anc_embeds3, pos_embeds3, neg_embeds3) / anc_embeds3.shape[0]
 		cl_loss = cal_infonce_loss(anc_embeds1, anc_embeds2, user_embeds2, self.temperature) + cal_infonce_loss(pos_embeds1, pos_embeds2, item_embeds2, self.temperature)
-		reg_loss = self.reg_weight * reg_pick_embeds([anc_embeds3, pos_embeds3, neg_embeds3])
+		cl_loss /= anc_embeds1.shape[0]
+		reg_loss = self.reg_weight * reg_params(self)
 		cl_loss *= self.cl_weight
 		loss = bpr_loss + reg_loss + cl_loss
 		losses = {'bpr_loss': bpr_loss, 'reg_loss': reg_loss, 'cl_loss': cl_loss}
