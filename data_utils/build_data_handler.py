739400b9a997ee576f9755a5d36189b4b33134c6
WW <39010092+weiwei1206@users.noreply.github.com>
2023-02-20 23:47:56 +0800

    Update build_data_handler.py

--- data_utils/build_data_handler.py
+++ data_utils/build_data_handler.py
@@ -6,14 +6,10 @@ def build_data_handler():
         data_handler = DataHandlerGeneralCF()
     elif configs['data']['type'] == 'sequential':
         data_handler = DataHandlerSequential()
-    elif configs['model']['name'] == 'cml':
-        data_handler = DataHandlerCML()
+    elif configs['data']['type'] == 'multi_behavior':
+        data_handler = DataHandlerMultiBehavior()
     elif configs['data']['type'] == 'social':
         data_handler = DataHandlerSocial()
-    elif configs['model']['name'] == 'mmclr':
-        data_handler = DataHandlerMMCLR()
-    elif configs['model']['name'] == 'hmgcr' or 'smbrec':
-        data_handler = DataHandlerHMGCRSMBRec()
     else:
         raise NotImplementedError
 
