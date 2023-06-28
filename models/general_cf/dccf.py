da79de0c4ee0260ecdc84f3bd6050b9bdf5a4046
Re-bin <1718954198@qq.com>
2023-06-28 21:58:49 +0800

    add dccf

---
+++ models/general_cf/dccf.py
@@ -0,0 +1,163 @@
+import torch
+import numpy as np
+import torch_sparse
+import torch.nn as nn
+import scipy.sparse as sp
+import torch.nn.functional as F
+
+from config.configurator import configs
+from models.general_cf.lightgcn import BaseModel
+from models.loss_utils import cal_bpr_loss, reg_params, cal_infonce_loss
+
+init = nn.init.xavier_uniform_
+uniformInit = nn.init.uniform
+
+class DCCF(BaseModel):
+    def __init__(self, data_handler):
+        super(DCCF, self).__init__(data_handler)
+
+        # prepare adjacency matrix for DCCF
+        rows = data_handler.trn_mat.tocoo().row
+        cols = data_handler.trn_mat.tocoo().col
+        new_rows = np.concatenate([rows, cols + self.user_num], axis=0)
+        new_cols = np.concatenate([cols + self.user_num, rows], axis=0)
+        plain_adj = sp.coo_matrix((np.ones(len(new_rows)), (new_rows, new_cols)), shape=[self.user_num + self.item_num, self.user_num + self.item_num]).tocsr().tocoo()
+        self.all_h_list = list(plain_adj.row)
+        self.all_t_list = list(plain_adj.col)
+        self.A_in_shape = plain_adj.shape
+        self.A_indices = torch.tensor([self.all_h_list, self.all_t_list], dtype=torch.long).cuda()
+        self.D_indices = torch.tensor([list(range(self.user_num + self.item_num)), list(range(self.user_num + self.item_num))], dtype=torch.long).cuda()
+        self.all_h_list = torch.LongTensor(self.all_h_list).cuda()
+        self.all_t_list = torch.LongTensor(self.all_t_list).cuda()
+        self.G_indices, self.G_values = self._cal_sparse_adj()
+
+        # hyper parameters
+        self.layer_num = configs['model']['layer_num']
+        self.intent_num = configs['model']['intent_num']
+        self.reg_weight = configs['model']['reg_weight']
+        self.cl_weight = configs['model']['cl_weight']
+        self.temperature = configs['model']['temperature']
+
+        # model parameters
+        self.user_embeds = nn.Embedding(self.user_num, self.embedding_size)
+        self.item_embeds = nn.Embedding(self.item_num, self.embedding_size)
+        self.user_intent = torch.nn.Parameter(init(torch.empty(self.embedding_size, self.intent_num)), requires_grad=True)
+        self.item_intent = torch.nn.Parameter(init(torch.empty(self.embedding_size, self.intent_num)), requires_grad=True)
+
+        # train/test
+        self.is_training = True
+        self.final_embeds = False
+
+    def _init_weight(self):
+        init(self.user_embeds.weight)
+        init(self.item_embeds.weight)
+
+    def _cal_sparse_adj(self):
+        A_values = torch.ones(size=(len(self.all_h_list), 1)).view(-1).cuda()
+        A_tensor = torch_sparse.SparseTensor(row=self.all_h_list, col=self.all_t_list, value=A_values, sparse_sizes=self.A_in_shape).cuda()
+        D_values = A_tensor.sum(dim=1).pow(-0.5)
+        G_indices, G_values = torch_sparse.spspmm(self.D_indices, D_values, self.A_indices, A_values, self.A_in_shape[0], self.A_in_shape[1], self.A_in_shape[1])
+        G_indices, G_values = torch_sparse.spspmm(G_indices, G_values, self.D_indices, D_values, self.A_in_shape[0], self.A_in_shape[1], self.A_in_shape[1])
+        return G_indices, G_values
+
+    def _adaptive_mask(self, head_embeddings, tail_embeddings):
+        head_embeddings = torch.nn.functional.normalize(head_embeddings)
+        tail_embeddings = torch.nn.functional.normalize(tail_embeddings)
+        edge_alpha = (torch.sum(head_embeddings * tail_embeddings, dim=1).view(-1) + 1) / 2
+        A_tensor = torch_sparse.SparseTensor(row=self.all_h_list, col=self.all_t_list, value=edge_alpha, sparse_sizes=self.A_in_shape).cuda()
+        D_scores_inv = A_tensor.sum(dim=1).pow(-1).nan_to_num(0, 0, 0).view(-1)
+        G_indices = torch.stack([self.all_h_list, self.all_t_list], dim=0)
+        G_values = D_scores_inv[self.all_h_list] * edge_alpha
+        return G_indices, G_values
+
+    def forward(self):
+        if not self.is_training and self.final_embeds is not None:
+            return self.final_embeds[:self.user_num], self.final_embeds[self.user_num:], None, None, None, None
+
+        all_embeds = [torch.concat([self.user_embeds.weight, self.item_embeds.weight], dim=0)]
+        gnn_embeds, int_embeds, gaa_embeds, iaa_embeds = [], [], [], []
+
+        for i in range(0, self.layer_num):
+            # Graph-based Message Passing
+            gnn_layer_embeds = torch_sparse.spmm(self.G_indices, self.G_values, self.A_in_shape[0], self.A_in_shape[1], all_embeds[i])
+
+            # Intent-aware Information Aggregation
+            u_embeds, i_embeds = torch.split(all_embeds[i], [self.user_num, self.item_num], 0)
+            u_int_embeds = torch.softmax(u_embeds @ self.user_intent, dim=1) @ self.user_intent.T
+            i_int_embeds = torch.softmax(i_embeds @ self.item_intent, dim=1) @ self.item_intent.T
+            int_layer_embeds = torch.concat([u_int_embeds, i_int_embeds], dim=0)
+
+            # Adaptive Augmentation
+            gnn_head_embeds = torch.index_select(gnn_layer_embeds, 0, self.all_h_list)
+            gnn_tail_embeds = torch.index_select(gnn_layer_embeds, 0, self.all_t_list)
+            int_head_embeds = torch.index_select(int_layer_embeds, 0, self.all_h_list)
+            int_tail_embeds = torch.index_select(int_layer_embeds, 0, self.all_t_list)
+            G_graph_indices, G_graph_values = self._adaptive_mask(gnn_head_embeds, gnn_tail_embeds)
+            G_inten_indices, G_inten_values = self._adaptive_mask(int_head_embeds, int_tail_embeds)
+            gaa_layer_embeds = torch_sparse.spmm(G_graph_indices, G_graph_values, self.A_in_shape[0], self.A_in_shape[1], all_embeds[i])
+            iaa_layer_embeds = torch_sparse.spmm(G_inten_indices, G_inten_values, self.A_in_shape[0], self.A_in_shape[1], all_embeds[i])
+
+            # Aggregation
+            gnn_embeds.append(gnn_layer_embeds)
+            int_embeds.append(int_layer_embeds)
+            gaa_embeds.append(gaa_layer_embeds)
+            iaa_embeds.append(iaa_layer_embeds)
+            all_embeds.append(gnn_layer_embeds + int_layer_embeds + gaa_layer_embeds + iaa_layer_embeds + all_embeds[i])
+
+        all_embeds = torch.stack(all_embeds, dim=1)
+        all_embeds = torch.sum(all_embeds, dim=1, keepdim=False)
+        user_embeds, item_embeds = torch.split(all_embeds, [self.user_num, self.item_num], 0)
+        self.final_embeds = all_embeds
+        return user_embeds, item_embeds, gnn_embeds, int_embeds, gaa_embeds, iaa_embeds
+
+    def _cal_cl_loss(self, users, items, gnn_emb, int_emb, gaa_emb, iaa_emb):
+        users = torch.unique(users)
+        items = torch.unique(items)
+        cl_loss = 0.0
+        for i in range(len(gnn_emb)):
+            u_gnn_embs, i_gnn_embs = torch.split(gnn_emb[i], [self.user_num, self.item_num], 0)
+            u_int_embs, i_int_embs = torch.split(int_emb[i], [self.user_num, self.item_num], 0)
+            u_gaa_embs, i_gaa_embs = torch.split(gaa_emb[i], [self.user_num, self.item_num], 0)
+            u_iaa_embs, i_iaa_embs = torch.split(iaa_emb[i], [self.user_num, self.item_num], 0)
+
+            u_gnn_embs = u_gnn_embs[users]
+            u_int_embs = u_int_embs[users]
+            u_gaa_embs = u_gaa_embs[users]
+            u_iaa_embs = u_iaa_embs[users]
+
+            i_gnn_embs = i_gnn_embs[items]
+            i_int_embs = i_int_embs[items]
+            i_gaa_embs = i_gaa_embs[items]
+            i_iaa_embs = i_iaa_embs[items]
+
+            cl_loss += cal_infonce_loss(u_gnn_embs, u_int_embs, u_int_embs, self.temperature) / u_gnn_embs.shape[0]
+            cl_loss += cal_infonce_loss(u_gnn_embs, u_gaa_embs, u_gaa_embs, self.temperature) / u_gnn_embs.shape[0]
+            cl_loss += cal_infonce_loss(u_gnn_embs, u_iaa_embs, u_iaa_embs, self.temperature) / u_gnn_embs.shape[0]
+            cl_loss += cal_infonce_loss(i_gnn_embs, i_int_embs, i_int_embs, self.temperature) / u_gnn_embs.shape[0]
+            cl_loss += cal_infonce_loss(i_gnn_embs, i_gaa_embs, i_gaa_embs, self.temperature) / u_gnn_embs.shape[0]
+            cl_loss += cal_infonce_loss(i_gnn_embs, i_iaa_embs, i_iaa_embs, self.temperature) / u_gnn_embs.shape[0]
+        return cl_loss
+
+    def cal_loss(self, batch_data):
+        self.is_training = True
+        user_embeds, item_embeds, gnn_embeds, int_embeds, gaa_embeds, iaa_embeds = self.forward()
+        ancs, poss, negs = batch_data
+        anc_embeds = user_embeds[ancs]
+        pos_embeds = item_embeds[poss]
+        neg_embeds = item_embeds[negs]
+        bpr_loss = cal_bpr_loss(anc_embeds, pos_embeds, neg_embeds) / anc_embeds.shape[0]
+        reg_loss = self.reg_weight * reg_params(self)
+        cl_loss = self.cl_weight * self._cal_cl_loss(ancs, poss, gnn_embeds, int_embeds, gaa_embeds, iaa_embeds)
+        loss = bpr_loss + reg_loss + cl_loss
+        losses = {'bpr_loss': bpr_loss, 'reg_loss': reg_loss, 'cl_loss': cl_loss}
+        return loss, losses
+
+    def full_predict(self, batch_data):
+        user_embeds, item_embeds, _, _, _, _ = self.forward()
+        self.is_training = False
+        pck_users, train_mask = batch_data
+        pck_users = pck_users.long()
+        pck_user_embeds = user_embeds[pck_users]
+        full_preds = pck_user_embeds @ item_embeds.T
+        full_preds = self._mask_predict(full_preds, train_mask)
+        return full_preds
