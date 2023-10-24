140fe98d72f4f5fc8449f8ecb90548b4b76118f3
yuh <yuhwhu@foxmail.com>
2023-10-24 21:23:28 +0800

    bugfix for seq data_utils

--- data_utils/data_handler_sequential.py
+++ data_utils/data_handler_sequential.py
@@ -13,6 +13,10 @@ class DataHandlerSequential:
         if configs['data']['name'] == 'ml-20m':
             predir = './datasets/sequential/ml-20m_seq/'
             configs['data']['dir'] = predir
+        elif configs['data']['name'] == 'sports':
+            predir = './datasets/sequential/sports_seq/'
+            configs['data']['dir'] = predir
+            
         self.trn_file = path.join(predir, 'train.tsv')
         self.val_file = path.join(predir, 'test.tsv')
         self.tst_file = path.join(predir, 'test.tsv')
